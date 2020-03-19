f2ba06ee576bb25135eb356ac078747120209bbd
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-19 17:12:21 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+#def reverse_each_word(sentence1)
+#  array = sentence1.split(' ')
+#  array.each { |element|  element.reverse }
+#end
+
+
+def reverse_each_word(string)
+  str = string.split(' ')
+  reversed = [ ]
+  str.each { |str| reversed << str.reverse + ' '}
+
+   reversed.join
+end
