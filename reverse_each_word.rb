3112783017dbf8c6ac3c8698f1908c72b1739abe
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-03-19 17:14:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@
 def reverse_each_word(string)
   str = string.split(' ')
   reversed = [ ]
-  str.each { |str| reversed << str.reverse}
+  str.collect { |str| reversed << str.reverse}
 
    p reversed.join(' ')
 end
